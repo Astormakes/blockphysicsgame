5932704881b41549d44696f3920374ae13f90856
Astormakes <astor111097@gmail.com>
2025-06-04 19:21:37 +0200

    fixed 1x1x1 slops, 2x1x1 slopes dont caiuse a crash

--- Scenes/Grid/GridRooms.gd
+++ Scenes/Grid/GridRooms.gd
@@ -44,7 +44,7 @@ func run_rooms():
 	var minY = minVec.y-1
 	var minZ = minVec.z-1
 	
-	
+	var blockvolume:Dictionary
 	var blocklist:Dictionary # this will save the "state" of each block and it will be the grid for the depth first search: it will step though this list using the same while true shit xyz... then when it found an room block it will disvorder the whole room. each block thats been found to be in this room, will be set to 0 in this list so its ignored in the future.
 	var airblock = [Vector3i.BACK,Vector3i.UP,Vector3i.LEFT,Vector3i.RIGHT,Vector3i.FORWARD,Vector3i.DOWN]
 	
@@ -58,14 +58,21 @@ func run_rooms():
 			if Part.type == "block": #  if its a solid block then it wont flow at all. add empty flowarray to blocklist  and go to next block
 				blocklist[pos] = []
 			else:
-				if Part.type == "reference": # if its a reference, like the tail of a slope, then it uses the secondary flow of that origin and rotates it to the blocks rotation same for below just without the reference
-					current = GlobalBlockManager.blockcatalog[Parts[Part.origin].blockid].flowsSecondary 
-				else:
-					current = GlobalBlockManager.blockcatalog[Part.blockid].flows
-				
 				blocklist[pos] = []
-				for flow in current: # this will take each block and its flows and rotate them to the angle of the block in the grid
-					blocklist[pos].append(flow * Part.position.basis.inverse()) 
+				if Part.type == "reference": # if its a reference, like the tail of a slope, then it uses the secondary flow of that origin and rotates it to the blocks rotation same for below just without the reference
+					blockvolume[pos] = GlobalBlockManager.blockcatalog[Parts[Part.origin].blockid].volume 
+					current = GlobalBlockManager.blockcatalog[Parts[Part.origin].blockid].flowsSecondary 
+					for flow in current: # this will take each block and its flows and rotate them to the angle of the block in the grid
+						blocklist[pos].append(Vector3i(flow * Parts[Part.origin].position.basis.inverse())) 
+				else:
+					blockvolume[pos] = GlobalBlockManager.blockcatalog[Part.blockid].volume 
+					current = GlobalBlockManager.blockcatalog[Part.blockid].flows
+					for flow in current: # this will take each block and its flows and rotate them to the angle of the block in the grid
+						blocklist[pos].append(Vector3i(flow * Part.position.basis.inverse())) 
+						
+				
+				
+				
 			
 		x += 1 
 		if x > maxX:
@@ -88,9 +95,9 @@ func run_rooms():
 	while(true): # this will actually discover the rooms.
 		var pos = Vector3i(x,y,z)
 		
-		#for test in blocklist.keys():
-		#	print(test," ",blocklist[test])
-		#print()
+		for test in blocklist.keys():
+			print(test," ",blocklist[test])
+		print()
 		
 		# new room discoverd
 		if blocklist[pos] != []: # if x,y,z has flows at all. add it to the tasklist
@@ -99,8 +106,8 @@ func run_rooms():
 			roomVolumes.append(0)
 			roomnumber = roomVolumes.size()-1
 			
-		print("roomnumber:",roomnumber)
-		print("___",Vector3i(x,y,z),"___")
+		#print("roomnumber:",roomnumber)
+		#print("___",Vector3i(x,y,z),"___")
 			
 			
 		while not tasklist.is_empty(): #if there are blocks of a room known work though them.
@@ -110,10 +117,9 @@ func run_rooms():
 			if (blocklist[taskpos] == []): continue # jump over entries that dont flow...
 			
 			if Parts.keys().has(taskpos):
-				#roomVolumes[roomnumber] += GlobalBlockManager.blockcatalog[Parts[taskpos].blockid].volume /8
-				roomVolumes[roomnumber] += 1
+				roomVolumes[roomnumber] += blockvolume[taskpos]
 			else:
-				roomVolumes[roomnumber] += 1
+				roomVolumes[roomnumber] += 8
 			
 			# this should cycle though the flow directions of the current block. if a block that flows right, is has a block on its right side that flows left then that block should be added to the tasklist as its connected .
 			for dirtemp:Vector3i in blocklist[taskpos]: 
@@ -121,15 +127,17 @@ func run_rooms():
 				if (checkpos.x < minX or checkpos.x > maxX or
 					checkpos.y < minY or checkpos.y > maxY or
 					checkpos.z < minZ or checkpos.z > maxZ):
-						print("ouf of bounds")
+						continue
+						#print("ouf of bounds")
 				else:
-					print("current block has:",blocklist[checkpos],"vs",dirtemp)
+					#print("current block has:",blocklist[checkpos],"vs",-dirtemp)
 					if blocklist[checkpos].has(-dirtemp):
 						tasklist.append(checkpos)
+						#print("added")
 			
 			blocklist[taskpos] = []
 			blockroom.set(taskpos,roomnumber)
-			print("done:",taskpos,",")
+			#print("done:",taskpos,",")
 		x += 1 
 		if x > maxX:
 			x = minX
