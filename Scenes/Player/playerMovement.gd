a1e1a8c757d16c6a400aabd4337490ca89379822
Astormakes <astor111097@gmail.com>
2025-07-20 09:41:51 +0200

    made movment simplified "noclip"
    when trying to errosion a major thing was when the fps would get too low the physics of the playerpill would freak out because of the exessive amount of dampening on the rotation of the player since the rotation of the camera was physics based ... thats gone now just plain god mode noclip it is for the moment in the future ill also not have it where the rotaoin is physics based because it was really not that good and made everything feel spongy... i might do the player pills movement though

--- Scenes/Player/playerMovement.gd
+++ Scenes/Player/playerMovement.gd
@@ -1,8 +1,8 @@
 extends RigidBody3D
 
-var speed := 300000
+var speed := 1
 var mouseSpeed = 0.15
-var runMul = 30
+var runMul = 10
 var movementDamping = Vector3(50000,300,50000)
 var Enginedelta = 0
 
@@ -17,40 +17,34 @@ func _process(delta: float) -> void:
 		Enginedelta = delta
 		
 		var inAir = 1
-		var velocity = Vector3(0,0,0)
+		var velocity:Vector3 = Vector3(0,0,0)
 		
 		if Input.is_action_pressed('forward'):
-				velocity.z -= speed
+				velocity.z = -speed
 		if Input.is_action_pressed('left'):
-				velocity.x -= speed
+				velocity.x = -speed
 		if Input.is_action_pressed('backward'):
-				velocity.z += speed
+				velocity.z = speed
 		if Input.is_action_pressed('right'):
-				velocity.x += speed
+				velocity.x = speed
 		if Input.is_action_pressed("run"):
 				velocity = velocity * runMul
 		if Input.is_action_pressed("jump"):
-				velocity.y += 500000
-		#if Input.is_action_just_pressed("debugg"):
-		#	var t = Terrain3D.data.get_height(Vector3(0,0,0))
-		#	print(t)
-		#	Terrain3D.data.set_height(Vector3(0,0,0),t+0.1)
-			
-			#for x in range(-range,range):
-				#for y in range(-range,range):
-					#GlobalSignals.signalGenereateNewRegion(transform.origin + Vector3(x,0,y)*512)
-
-
-
-		$".".set_constant_force(delta*(velocity.rotated(Vector3.UP,$".".rotation.y)*inAir-$".".linear_velocity*movementDamping*inAir)) # this is a dampener
+				velocity.y = 5
+		if Input.is_action_pressed("crouch"):
+				velocity.y = -5
+	
+	
+		transform.origin += Head.basis * velocity
 		
-		$".".apply_torque_impulse(delta*Vector3(0,-$".".angular_velocity.y*300,0))  #this is dampening for the rotation of the player pill
+
 
 func _input(event):
 	if not Input.is_action_pressed("free_mouse"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		if event is InputEventMouseMotion:
-			Head.rotation_degrees.x = clamp(Head.rotation_degrees.x - event.relative.y * mouseSpeed,-80,90) # this rotates the players head
-			$".".apply_torque_impulse(Enginedelta*Vector3(0,-event.relative.x*mouseSpeed*500,0)) # this rotates the playerpill based on mouseinput
+			Head.rotation_degrees.x = clamp(Head.rotation_degrees.x - event.relative.y * mouseSpeed,-89,89) # this rotates the players head
+			Head.rotation_degrees.y = Head.rotation_degrees.y - event.relative.x * mouseSpeed # this rotates the players head
+			
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # this is what uncatches the mouse to close the game using alt key for example
